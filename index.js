b015012767da5381bca83838e0b6fd73fc42bc3e
joepamedia <josephpalmisano18@gmail.com>
2022-02-26 14:29:11 -0600

    html generated w/ cards

---
+++ index.js
@@ -0,0 +1,195 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+// value is fixed as it's referenced in multiple locations
+const MANAGER_TYPE = "Manager";
+const ENGINEER_TYPE = "Engineer";
+const INTERN_TYPE = "Intern";
+const employeeArray = [];
+// functions
+const mainMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "option",
+        message: "What do you want to do next?",
+        choices: ["Add an Engineer.", "Add an Intern.", "I am done building my team."],
+      },
+    ])
+    .then((answers) => {
+      // question response answers
+
+      switch (answers.option) {
+        case "Add an Engineer.":
+          // run code to add engineer
+          collectEmployeeInfo(ENGINEER_TYPE);
+          break;
+        case "Add an Intern.":
+          collectEmployeeInfo(INTERN_TYPE);
+          break;
+        case "I am done building my team.":
+          // run code to finish/generate HTML
+          createHTML(employeeArray);
+          break;
+
+        default:
+          throw new Error("Option not recognized");
+          break;
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        console.log(error); // Something else went wrong
+      }
+    });
+};
+
+// use inquirer here to ask questions and create html
+const collectEmployeeInfo = (employeeType) => {
+  inquirer
+    // asks questions in terminal
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your employee's email?",
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        when: () => employeeType === MANAGER_TYPE,
+        message: "What is your employee's office number?",
+      },
+      {
+        type: "input",
+        name: "github",
+        when: () => employeeType === ENGINEER_TYPE,
+        message: "What is your employee's Github username?",
+      },
+      {
+        type: "input",
+        name: "school",
+        when: () => employeeType === INTERN_TYPE,
+        message: "Where does your employee go to school?",
+      },
+    ])
+    .then((answers) => {
+      // question response answers
+
+      switch (employeeType) {
+        case MANAGER_TYPE:
+          // create manager object
+          const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
+          employeeArray.push(manager);
+          mainMenu();
+          break;
+        case ENGINEER_TYPE:
+          // create "Engineer" object
+          const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+          employeeArray.push(engineer);
+          mainMenu();
+          break;
+        case INTERN_TYPE:
+          // create intern object
+          const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+          employeeArray.push(intern);
+          mainMenu();
+          break;
+
+        default:
+          throw new Error("Option not recognized");
+          break;
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+  //destructured
+};
+
+// ADD CONDITIONALS, ADD DIFFERENT LOOPS FOR EACH TYPE OF PERSON
+function generateSpecial(worker) {
+  if (worker.getRole() === MANAGER_TYPE) {
+    return `<li class="list-group-item">${worker.getOfficeNum()}</li>;`;
+  }
+  if (worker.getRole() === ENGINEER_TYPE) {
+    return `<li class="list-group-item">${worker.getGithub()}</li>;`;
+  } else {
+    return `<li class="list-group-item">${worker.getSchool()}</li>;`;
+  }
+}
+
+function makeCard(workers) {
+  let managerCardHTML = "";
+  // loop. ADD CONDITIONAL
+  workers.forEach((worker) => {
+    // manager card
+    managerCardHTML += `<div class="card bg-dark mb-3 card-spacing" style="width: 18rem">
+    <div class="card-header">${worker.getRole()}</div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${worker.getName()}</li>
+      <li class="list-group-item">${worker.getId()}</li>
+      <li class="list-group-item">${worker.getEmail()}</li>
+      ${generateSpecial(worker)}
+    </ul>
+  </div>`;
+  });
+  return managerCardHTML;
+}
+
+const createHTML = (employees) => {
+  // pass an array of employees
+  const htmlFile = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Team Profile Generator</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    </head>
+    <body>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4 d-f">Team Profile Generator</h1>
+        </div>
+      </div>
+      ${makeCard(employees)}
+    </body>
+    </html>`;
+  console.log(htmlFile);
+  fs.writeFile("./dist/index.html", htmlFile, (err) => (err ? console.log(err) : console.log("Successfully created index.html!")));
+};
+
+collectEmployeeInfo(MANAGER_TYPE, ENGINEER_TYPE, INTERN_TYPE);
+
+// we want to create a card for each employee created starting w/ manager
+//collect employee information
+//    collect manager info
+//    ask user what to do next
+//      add new team member
+//        collect team member info
+//
+//      or finish
+// create employee cards
+// create html document
